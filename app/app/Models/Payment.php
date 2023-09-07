afa706ea2bc2881ea760bf9d43ab497ce9625459
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-09-07 16:28:24 +0900

    add route payments

--- app/app/Models/Payment.php
+++ app/app/Models/Payment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'summary_ym',
+        'group_id',
+        'member_id',
+        'category_id',
+        'income_flg',
+        'categorized_payment_id',
+        'payment_date',
+        'amount',
+        'payment_label',
+        'del_flg'
+    ];
 }
